2c153c9b2daf0e2b0df94d97a09d7d158ef58f09
nikhil24062000 <105099751+nikhil24062000@users.noreply.github.com>
2022-05-08 14:04:03 +0530

    Update 00_uncleanCode_pylint.py

--- 00_uncleanCode_pylint.py
+++ 00_uncleanCode_pylint.py
@@ -1,24 +1,24 @@
 "hello"
 import string
 
-shift = 3
-choice = input("would you like to encode or decode")
-word = input("Please enter text")
-letters = string.ascii_letters + string.punctuation + string.digits
-encoded = ''
-if choice == "encode":
-    for letter in word:
+SHIFT = 3
+CHOICE = input("would you like to encode or decode")
+WORD = input("Please enter text")
+LETTERS = string.ascii_letters + string.punctuation + string.digits
+ENCODED = ''
+if CHOICE == "encode":
+    for letter in WORD:
         if letter == ' ':
-            encoded = encoded + ' '
+            ENCODED = ENCODED + ' '
         else:
-            x = letters.index(letter) + shift
-            encoded = encoded + letters[x]
-if choice == "decode":
-    for letter in word:
+            x = LETTERS.index(letter) + SHIFT
+            ENCODED = ENCODED + LETTERS[x]
+if CHOICE == "decode":
+    for letter in WORD:
         if letter == ' ':
-            encoded = encoded + ' '
+            ENCODED = ENCODED + ' '
         else:
-            x = letters.index(letter) - shift
-            encoded = encoded + letters[x]
+            x = LETTERS.index(letter) - SHIFT
+            ENCODED = ENCODED + LETTERS[x]
 
-print(encoded)
+print(ENCODED)
